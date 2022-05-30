fadf4bb1346cc815cfe2c27de2018db2eda6c28c
arteesan <gamearteesan@gmail.com>
2022-05-29 23:29:15 -0400

    links

--- src/components/social-media.vue
+++ src/components/social-media.vue
@@ -1,6 +1,6 @@
 <template>
     <a class="nodeco letter-s-3 noborder n-tx tx-lg n-conve clickable flex-center mx-1"
-        href="https://t.me/"  target="_blank"
+        href="https://twitter.com/arteesan"  target="_blank"
         style="border-radius: 0 0 30px 30px"
     >
         <span class="px-3 pb-4 pt-3 opacity-hover-50">
@@ -16,7 +16,7 @@
         </span>
     </a>
     <a class="nodeco letter-s-3 noborder n-tx tx-lg n-conve clickable flex-center mx-1"
-        href="https://t.me/"  target="_blank"
+        href="https://github.com/arteesanio/OpenFarm-WebDApp-V1/"  target="_blank"
         style="border-radius: 0 0 30px 30px"
     >
         <span class="px-3 pb-4 pt-3 opacity-hover-50">
